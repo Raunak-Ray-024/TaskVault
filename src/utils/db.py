b2e3e4b66fe9de2cc0914586cbc9889dd11e2bb5
Raunak-Ray-024 <sanjanarintu@gmail.com>
2026-05-29 14:28:06 +0530

    done

--- src/utils/db.py
+++ src/utils/db.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine  #create_engine= creates DB Connection
 from sqlalchemy.orm import sessionmaker,declarative_base  #sessions- create DB session and declarative_base= base class fro models
 from src.utils.settings import settings  #contains config like DB URL
 Base=declarative_base()  #parent class for all ORM MODels
-engine=create_engine(url=settings.DB_CONNECTION)  #establiishes connection to your DB
+engine=create_engine(url=settings.DB_CONNECTION,pool_pre_ping=True, 
+    # 2. Recycles connections every 10 minutes (600s) before cloud thresholds drop them
+    pool_recycle=600,   
+    # 3. Keeps a stable pool size suitable for low-tier cloud instances
+    pool_size=5,        
+    max_overflow=10)  #establiishes connection to your DB
 
 LocalSession=sessionmaker(bind=engine) #creates session objects
 #A transactional workspace to query/update DB
